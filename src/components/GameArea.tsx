f7274f2a65e10d6a7930b047abb69be9bb046677
Shane <ShaneButtCode@gmail.com>
2021-02-17 10:00:55 -0330

    Set up discard and draw functions

--- src/components/GameArea.tsx
+++ src/components/GameArea.tsx
@@ -1,6 +1,13 @@
-import { Card, Deck } from "../functions/Deck";
-
+import { useEffect, useState } from "react";
+import { Deck } from "../functions/Deck";
+let deck = new Deck(false);
+let discardPile = new Deck();
 function GameArea(props: any) {
+  //Round of play
+  let [turn, setTurn] = useState(1);
+  //Player turn in round
+  let [playerTurn, setPlayerTurn] = useState(0);
+  //The Deck compriseing of one deck of standard playing cards with no jokers
   let game = (
     startCash: Number,
     minBet: Number,
@@ -9,12 +16,15 @@ function GameArea(props: any) {
     setHeldCash: Function
   ) => {
     //The Deck compriseing of one deck of standard playing cards with no jokers
-    let deck = new Deck(false);
     //initial Shuffling of the deck
+    if (deck.size() !== 52) {
+      deck = new Deck(false);
+      discardPile = new Deck();
+    }
     for (let i = 0; i < 5; i++) {
       deck.shuffle();
     }
-    //Crate hands for each player
+    //initial Create hands for each player
     let hands = [];
     let cash = [];
     for (let i = 0; i < numOfPlayers; i++) {
@@ -36,22 +46,54 @@ function GameArea(props: any) {
     hands = null;
     cash = null;
   };
+
+  /**
+   * Draws a card from deck if posible if can't adds discard pile to deck shuffles and then draws
+   */
+  const draw = () => {
+    if (!deck.canDraw()) {
+      deck.addDeckToBottom(discardPile);
+      discardPile = new Deck();
+      deck.shuffle();
+    }
+    return deck.draw();
+  };
+
+  /**
+   * discards selected cards from a player's hand
+   * then drawscards to replace him
+   * @param cardPositions The 0 indexed position of cards to be replaced
+   */
+  const discardAndDraw = (cardPositions: Array<Number>) => {
+    let hands = [...props.heldCards];
+    for (let pos of cardPositions) {
+      let card = draw();
+      discardPile.addToTop(hands[playerTurn][+pos]);
+      hands[playerTurn][+pos] = card;
+    }
+    //Set State
+    props.setHeldCards(hands);
+    //return mem
+    hands = null;
+  };
+
+  //Delete later used for testing
+  const testRemove = () => {
+    discardAndDraw([0, 2, 4]);
+  };
+
+  useEffect(() => {
+    game(
+      props.startMoney,
+      props.minBet,
+      props.numOfPlayers,
+      props.setHeldCards,
+      props.setHeldCash
+    );
+  }, [props.numOfPlayers, props.startMoney, props.minBet]);
   return (
     <div>
       Test Text
-      <div
-        onClick={() =>
-          game(
-            props.startMoney,
-            props.minBet,
-            props.numOfPlayers,
-            props.setHeldCards,
-            props.setHeldCash
-          )
-        }
-      >
-        Click to start simulation
-      </div>
       <br />
       NumberOfPlayers:{props.numOfPlayers}
       <br />
@@ -59,7 +101,17 @@ function GameArea(props: any) {
       <br />
       MinBet:{props.minBet}
       <br />
+      Deck:{deck.size()} Discard:{discardPile.size()}
+      <br />
       Each players hand and cash: {props.heldCash[0]}
+      <br />
+      <div
+        onClick={() => {
+          testRemove();
+        }}
+      >
+        Click to remove 1st, 3rd, 5th card from p1 hand
+      </div>
       <br />
       <ol>
         {props.heldCards.map((hand, n) => {
